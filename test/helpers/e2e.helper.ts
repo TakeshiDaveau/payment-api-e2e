145cc7ee8a8a598b3c38c1598abe1ec1b6402c13
TakeshiDaveau <takeshi.daveau@prestashop.com>
2023-03-14 09:57:19 +0100

    fix: remove console.log

--- test/helpers/e2e.helper.ts
+++ test/helpers/e2e.helper.ts
@@ -114,12 +114,6 @@ export class E2eHelper {
     }> = await Promise.all(
       providers.map((provider) => this.app.resolve(provider)),
     );
-    console.log(`Bellow the list of modules`);
-    console.log(E2eHelper.modulesDefinition);
-    console.log(`Bellow the list of providers`);
-    console.log(providers);
-    console.log(`Bellow the list of resolved providers`);
-    console.log(resolvedProviders);
     await Promise.all(
       // Retrieve the provider
       await resolvedProviders
